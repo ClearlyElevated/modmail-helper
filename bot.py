3faf4bb4f827547cd959970e72b16c128401f38a
taaku251 <45324516+Taaku18@users.noreply.github.com>
2019-01-18 22:50:27 -0800

    Remove class structure

--- bot.py
+++ bot.py
@@ -4,43 +4,43 @@ from discord.ext import commands
 from aiohttp import ClientSession
 
 
-class Helper(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.session = ClientSession(loop=self.loop)
-        self.url = f"https://api.heroku.com/apps/{getenv('APP_NAME')}/builds"
-        self.headers = {
-            'Accept': 'application/vnd.heroku+json; version=3',
-            'Content-Type': 'application/json',
-            'Authorization': f"Bearer {getenv('HEROKU_TOKEN')}"
+bot = commands.Bot(command_prefix='%')
+
+
+session = ClientSession(loop=bot.loop)
+url = f"https://api.heroku.com/apps/{getenv('APP_NAME')}/builds"
+headers = {
+    'Accept': 'application/vnd.heroku+json; version=3',
+    'Content-Type': 'application/json',
+    'Authorization': f"Bearer {getenv('HEROKU_TOKEN')}"
+}
+
+
+def get_payload(version):
+    m = match(r'(v?[\d.-]+)', str(version))
+    if m is None:
+        raise commands.BadArgument('Invalid Version')
+    version = m.group(0)
+    if not version.startswith('v'):
+        version = 'v' + version
+
+    return {
+        'source_blob': {
+            'url': 'https://github.com/kyb3r/modmail/'
+                   f'archive/{version}.tar.gz'
         }
-
-    @staticmethod
-    def get_payload(version):
-        m = match(r'(v?[\d.-]+)', str(version))
-        if m is None:
-            raise commands.BadArgument('Invalid Version')
-        version = m.group(0)
-        if not version.startswith('v'):
-            version = 'v' + version
-
-        return {
-            'source_blob': {
-                'url': 'https://github.com/kyb3r/modmail/'
-                       f'archive/{version}.tar.gz'
-            }
-        }
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def checkout(self, ctx, version):
-
-        async with self.session.post(url=self.url, headers=self.headers,
-                                     json=self.get_payload(version)) as r:
-            msg = await r.json()
-            await ctx.send(msg)
-            print(msg)
+    }
 
 
-bot = Helper(command_prefix='%')
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def checkout(ctx, version):
+
+    async with session.post(url=url, headers=headers,
+                            json=get_payload(version)) as r:
+        msg = await r.json()
+        await ctx.send(msg)
+        print(msg)
+
+
 bot.run(getenv('BOT_TOKEN'))
