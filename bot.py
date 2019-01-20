a95f9aea14314e23ca57b34633d80628326dd516
taaku251 <45324516+Taaku18@users.noreply.github.com>
2019-01-19 21:08:39 -0800

    Support checkout commit sha

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 from os import getenv
 from re import match
 from discord.ext import commands
-from discord import Embed, Game
+from discord import Embed, Game, Color
 from aiohttp import ClientSession
 from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
+
+load_dotenv()
 
 
 bot = commands.Bot(command_prefix='$')
@@ -16,9 +17,17 @@ github_base = 'https://api.github.com/repos/kyb3r/modmail'
 
 build_url = heroku_base + '/builds'
 versions_url = github_base + '/tags'
-latest_url = github_base + '/releases/latest'
+latest_release_url = github_base + '/releases/latest'
+latest_commit_url = github_base + '/commits'
 restart_url = heroku_base + '/dynos'
 config_url = heroku_base + '/config-vars'
+tarball_url = github_base + '/tarball/{sha}'
+
+# from https://github.com/semver/semver/issues/232
+semantic_version_regex = r'^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[' \
+                         r'1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[' \
+                         r'1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[' \
+                         r'0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$'
 
 headers = {
     'Accept': 'application/vnd.heroku+json; version=3',
@@ -33,15 +42,31 @@ async def on_ready():
     await bot.change_presence(activity=Game('$help for commands!'))
 
 
+@bot.event
+async def on_command_error(ctx: commands.Context,
+                           error: commands.CommandError):
+    if isinstance(error, commands.BadArgument):
+        return await ctx.send(embed=Embed(
+            title="Failed",
+            color=Color.red(),
+            description=error.args[0]
+        ))
+
+
 async def get_versions():
     async with session.get(versions_url) as resp:
         data = await resp.json()
         return {version['name']: version['tarball_url'] for version in data}
 
 
-async def get_latest():
-    async with session.get(url=latest_url) as resp:
+async def get_latest_release():
+    async with session.get(latest_release_url) as resp:
         return (await resp.json()).get('tag_name')
+
+
+async def get_latest_commit():
+    async with session.get(latest_commit_url) as resp:
+        return (await resp.json())[0]['sha']
 
 
 @bot.command(name='versions')
@@ -62,6 +87,9 @@ async def versions_(ctx: commands.Context):
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def restart(ctx: commands.Context):
+    """
+    Restarts the bot.
+    """
     async with session.delete(restart_url, headers=headers) as resp:
         status = 'Success' if str(resp.status).startswith('2') else 'Failed'
         return await ctx.send(embed=Embed(title=status))
@@ -73,29 +101,42 @@ async def checkout(ctx: commands.Context, *, version: str):
     """
     Checkout a specific version.
 
-    See "$versions" for a list of versions or
-    "$checkout latest" for the latest version.
+    See "$versions" for a list of versions.
+
+     - "$checkout latest release" for the latest release.
+     - "$checkout latest" or "$checkout latest commit" for the latest commit.
     """
-    if version == 'latest':
-        version = await get_latest()
-
     versions = await get_versions()
-    m = match(r'(v?[\d.\w\-]+)', str(version))
-    if m is None:
-        raise commands.BadArgument('Invalid Version')
-    version = m.group(0)
-    if not version.startswith('v'):
-        version = 'v' + version
 
-    if version not in versions:
-        return await ctx.send(embed=Embed(
-            title="Invalid Version",
-            description=f'Cannot find {version}'
-        ))
+    if version == 'latest release':
+        version = await get_latest_release()
+        url = versions[version]
+
+    elif version == 'latest commit':
+        sha = await get_latest_commit()
+        url = tarball_url.format(sha=sha)
+
+    else:
+        m = match(semantic_version_regex, version)
+        if m is None:
+            m = match(r'^[a-z\d]{7,}$', version)
+            if m is None:
+                raise commands.BadArgument('Invalid Version/Commit SHA.')
+            sha = m.group(0)
+            url = tarball_url.format(sha=sha)
+        else:
+            version = m.group(0)
+            if not version.startswith('v'):
+                version = 'v' + version
+
+            if version not in versions:
+                raise commands.BadArgument(f'Cannot find version: {version}.')
+
+            url = versions[version]
 
     payload = {
         'source_blob': {
-            'url': versions[version]
+            'url': url
         }
     }
     async with session.post(build_url, headers=headers,
@@ -126,10 +167,11 @@ async def checkout(ctx: commands.Context, *, version: str):
                         current = '...'
                     em = Embed(title='Output Stream', description=current)
                     await msg.edit(embed=em)
+
         if msg.embeds[0].description == 'starting...':
             em = Embed(title='Output Stream',
                        description=f'Failed to stream output. {output_url}')
-            await msg.edit(em)
+            await msg.edit(embed=em)
 
 
 async def send_env(payload):
@@ -142,6 +184,9 @@ async def send_env(payload):
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def setenv(ctx: commands.Context, key: str, *, value: str):
+    """
+    Sets an environment variable.
+    """
     payload = {
         key: value
     }
@@ -154,6 +199,9 @@ async def setenv(ctx: commands.Context, key: str, *, value: str):
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def rmenv(ctx: commands.Context, key: str):
+    """
+    Removes an environment variable.
+    """
     payload = {
         key: None
     }
@@ -166,6 +214,9 @@ async def rmenv(ctx: commands.Context, key: str):
 @bot.command(name='getenv')
 @commands.has_permissions(administrator=True)
 async def getenv_(ctx: commands.Context):
+    """
+    View all environment variables.
+    """
     async with session.get(config_url, headers=headers) as resp:
         envs = await resp.json()
         em = Embed(title='Environment Variables:')
